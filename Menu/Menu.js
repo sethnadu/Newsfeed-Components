a4070c3a23aed3323e9c714e567621440fd52bd4
Seth Nadu <seth.nadu@gmail.com>
2019-07-10 16:58:03 -0400

    menu finished

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,6 +9,7 @@ let menuItems = [
   'Log Out'
 ];
 
+
 /* 
 
   Step 1: Write a function that will create a menu component as seen below:
@@ -33,3 +34,56 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+const menuButton = document.querySelector(".menu-button");
+const header = document.querySelector(".header");
+
+header.appendChild(createNav(menuItems));
+
+
+
+function createNav(menuItems) {
+  const menu = document.createElement("div");
+  const list = document.createElement("ul");
+  const item1 = document.createElement('li');
+  const item2 = document.createElement('li');
+  const item3 = document.createElement('li');
+  const item4 = document.createElement('li');
+  const item5 = document.createElement('li');
+  const item6 = document.createElement('li');
+
+  menu.appendChild(list);
+  list.appendChild(item1);
+  list.appendChild(item2);
+  list.appendChild(item3);
+  list.appendChild(item4);
+  list.appendChild(item5);
+  list.appendChild(item6);
+  
+  menu.classList.add('menu');
+  
+
+  item1.textContent = menuItems[0];
+  item2.textContent = menuItems[1];
+  item3.textContent = menuItems[2];
+  item4.textContent = menuItems[3];
+  item5.textContent = menuItems[4];
+  item6.textContent = menuItems[5];
+
+  console.log(item1);
+  console.log(item2);
+  console.log(item3);
+  console.log(item4);
+  console.log(item5);
+  console.log(item6);
+
+  
+
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("menu--open")
+  });
+
+
+  return menu;
+
+}
